559aa67e01ae9dec88ebd00c6b25d11e00e45586
Ocean Basher <oguzhanbaserw@gmail.com>
2021-03-07 16:34:01 +0300

    php update

---
+++ assets/php/sendemail.php
@@ -0,0 +1,19 @@
+<?php
+       // from the form
+       $name = trim(strip_tags($_POST['name']));
+       $email = trim(strip_tags($_POST['email']));
+       $subject = trim(strip_tags($_POST['subject']));
+       $message = htmlentities($_POST['message']);
+
+       $to = 'oguzhanbaserw@gmail.com';
+
+       $body = <<<HTML
+$message
+HTML;
+
+       $headers = "From: $email\r\n";
+       $headers .= "Content-type: text/html\r\n";
+
+       // send the email
+       mail($to, $subject, $body, $headers);
+?>
